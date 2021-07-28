a191d68f1bef67f99be43b75cc2f160bd270d821
juanito87 <juancruz.f87@gmail.com>
2021-07-28 11:34:20 -0300

    Updating prompt to show venv. Need to get a cleaner look

--- prompt/prompt-gruvbox.sh
+++ prompt/prompt-gruvbox.sh
@@ -1,6 +1,7 @@
 #
 # This shell prompt config file was created by promptline.vim
-#
+# Then adapted by Juanito
+
 function __promptline_host {
   local only_if_ssh="0"
 
@@ -15,6 +16,7 @@ function __promptline_last_exit_code {
 
   printf "%s" "$last_exit_code"
 }
+
 function __promptline_ps1 {
   local slice_prefix slice_empty_prefix slice_joiner slice_suffix is_prompt_empty=1
 
@@ -28,7 +30,7 @@ function __promptline_ps1 {
   slice_prefix="${b_bg}${sep}${b_fg}${b_bg}${space}" slice_suffix="$space${b_sep_fg}" slice_joiner="${b_fg}${b_bg}${alt_sep}${space}" slice_empty_prefix="${b_fg}${b_bg}${space}"
   [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
   # section "b" slices
-  __promptline_wrapper "$USER" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+  __promptline_wrapper "$USER"  "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
 
   # section "c" header
   slice_prefix="${c_bg}${sep}${c_fg}${c_bg}${space}" slice_suffix="$space${c_sep_fg}" slice_joiner="${c_fg}${c_bg}${alt_sep}${space}" slice_empty_prefix="${c_fg}${c_bg}${space}"
@@ -40,7 +42,7 @@ function __promptline_ps1 {
   slice_prefix="${y_bg}${sep}${y_fg}${y_bg}${space}" slice_suffix="$space${y_sep_fg}" slice_joiner="${y_fg}${y_bg}${alt_sep}${space}" slice_empty_prefix="${y_fg}${y_bg}${space}"
   [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
   # section "y" slices
-  __promptline_wrapper "$(__promptline_vcs_branch)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+  __promptline_wrapper "$(__promptline_vcs_branch)" "$slice_prefix" "$slice_suffix"  && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
 
   # section "warn" header
   slice_prefix="${warn_bg}${sep}${warn_fg}${warn_bg}${space}" slice_suffix="$space${warn_sep_fg}" slice_joiner="${warn_fg}${warn_bg}${alt_sep}${space}" slice_empty_prefix="${warn_fg}${warn_bg}${space}"
@@ -48,9 +50,25 @@ function __promptline_ps1 {
   # section "warn" slices
   __promptline_wrapper "$(__promptline_last_exit_code)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
 
+# section "down" header
+slice_prefix="${a_bg}${sep}${a_fg}${a_bg}${space}" slice_suffix="$space${a_sep_fg}" slice_joiner="${a_fg}${a_bg}${alt_sep}${space}" slice_empty_prefix="${a_fg}${a_bg}${space}"
+[ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
+# section "down" slices
+__promptline_wrapper "$(__promptline_venv)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+
   # close sections
   printf "%s" "${reset_bg}${sep}$reset$space"
 }
+
+function __promptline_venv {
+    if test -z "$VIRTUAL_ENV" ; then
+        echo ""
+    else
+        echo $VIRTUAL_ENV
+    fi
+    return 1
+}
+
 function __promptline_vcs_branch {
 git_symbol_synced=''
 git_symbol_unpushed=' ▲'
@@ -112,8 +130,8 @@ git_branch_symbol=" "
 
             ## RETURN STRING
             echo "$git_branch_symbol$branch$symbol"
-            fi
-fi
+        fi
+    fi
 ## DEFAULT
 echo ""
 return 1
@@ -150,6 +168,7 @@ function __promptline_cwd {
 
   printf "%s" "$first_char$formatted_cwd"
 }
+
 function __promptline_left_prompt {
   local slice_prefix slice_empty_prefix slice_joiner slice_suffix is_prompt_empty=1
 
@@ -174,6 +193,7 @@ function __promptline_left_prompt {
   # close sections
   printf "%s" "${reset_bg}${sep}$reset$space"
 }
+
 function __promptline_wrapper {
   # wrap the text in $1 with $2 and $3, only if $1 is not empty
   # $2 and $3 typically contain non-content-text, like color escape codes and separators
@@ -181,6 +201,7 @@ function __promptline_wrapper {
   [[ -n "$1" ]] || return 1
   printf "%s" "${2}${1}${3}"
 }
+
 function __promptline_right_prompt {
   local slice_prefix slice_empty_prefix slice_joiner slice_suffix
 
@@ -197,6 +218,17 @@ function __promptline_right_prompt {
   # close sections
   printf "%s" "$reset"
 }
+
+function __promptline_down_prompt {
+  local slice_prefix slice_empty_prefix slice_joiner slice_suffix
+
+    # section "down" header
+    slice_prefix="${a_bg}${sep}${a_fg}${a_bg}${space}" slice_suffix="$space${a_sep_fg}" slice_joiner="${a_fg}${a_bg}${alt_sep}${space}" slice_empty_prefix="${a_fg}${a_bg}${space}"
+    [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
+    # section "down" slices
+__promptline_wrapper "$(__promptline_venv)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+}
+
 function __promptline {
   local last_exit_code="${PROMPTLINE_LAST_EXIT_CODE:-$?}"
 
@@ -244,7 +276,8 @@ function __promptline {
       __promptline_ps1
     fi
   else
-    PS1="$(__promptline_ps1)\n${sep}"
+      PS1="$(__promptline_ps1)\n${sep}"
+      # PS1="$(__promptline_ps1)\n$(__promptline_down_prompt)"
   fi
 }
 
